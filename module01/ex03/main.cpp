bcdc27ef77b6cc09a0c367ce3b3c3f1fe2655a62
Chris Won <chris@debian>
2025-08-16 01:18:07 +0700

    ex03 complete

--- module01/ex03/main.cpp
+++ module01/ex03/main.cpp
@@ -6,25 +6,30 @@
 /*   By: cwon <cwon@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 01:13:38 by cwon              #+#    #+#             */
-/*   Updated: 2025/08/16 01:13:49 by cwon             ###   ########.fr       */
+/*   Updated: 2025/08/16 01:16:29 by cwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "HumanA.hpp"
+#include "HumanB.hpp"
 
 int main()
 {
   {
-  Weapon club = Weapon("crude spiked club");
-  HumanA bob("Bob", club);
-  bob.attack();
-  club.setType("some other type of club");
-  bob.attack();
-  }{
-  Weapon club = Weapon("crude spiked club");
-  HumanB jim("Jim");
-  jim.setWeapon(club);
-  jim.attack();
-  club.setType("some other type of club");
-  jim.attack();
+    Weapon club = Weapon("crude spiked club");
+    HumanA bob("Bob", club);
+    bob.attack();
+    club.setType("some other type of club");
+    bob.attack();
   }
+  {
+    Weapon club = Weapon("crude spiked club");
+    HumanB jim("Jim");
+    jim.setWeapon(club);
+    jim.attack();
+    club.setType("some other type of club");
+    jim.attack();
+  }
+  
   return 0;
 }
