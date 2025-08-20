1aa539c83c886c3c9e4c3c210c264fb5765c9f4e
Chris Won <chrisdongwon@gmail.com>
2025-08-20 08:13:21 +0700

    Update Harl.cpp
    dialogues pasted from the subject pdf

--- module01/ex05/Harl.cpp
+++ module01/ex05/Harl.cpp
@@ -17,17 +17,20 @@ void Harl::complain(std::string level) {
 }
 
 void Harl::debug( void ) {
-
+  "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup
+burger. I really do!"
 }
 
 void Harl::error( void ) {
-
+"This is unacceptable! I want to speak to the manager now."
 }
 
 void Harl::info ( void ) {
-
+  "I cannot believe adding extra bacon costs more money. You didn’t put
+enough bacon in my burger! If you did, I wouldn’t be asking for more!"
 }
 
 void Harl::warning( void ) {
-  
+  "I think I deserve to have some extra bacon for free. I’ve been coming for
+years, whereas you started working here just last month."
 }
