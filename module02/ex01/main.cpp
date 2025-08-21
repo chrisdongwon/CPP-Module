14653c357578b610e317a58be61a3926d84822cd
Chris Won <chris@debian>
2025-08-21 09:57:16 +0700

    module02 name fixed and ex01 completed

---
+++ module02/ex01/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwon <cwon@student.42bangkok.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 09:53:17 by cwon              #+#    #+#             */
+/*   Updated: 2025/08/21 09:53:17 by cwon             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+  Fixed a;
+  Fixed const b( 10 );
+  Fixed const c( 42.42f );
+  Fixed const d( b );
+
+  a = Fixed( 1234.4321f );
+
+  std::cout << "a is " << a << std::endl;
+  std::cout << "b is " << b << std::endl;
+  std::cout << "c is " << c << std::endl;
+  std::cout << "d is " << d << std::endl;
+
+  std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+  std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+  std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+  std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+  return 0;
+}
