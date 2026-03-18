5989fe8441241ac702914ad283ab60bbe7135322
Chris Won <chriswon@GJ6L59PYX5-3.local>
2026-03-18 13:25:09 +0700

    module 05 started, ex00 finished

---
+++ module05/ex00/Bureaucrat.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwon <cwon@student.42bangkok.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 12:45:13 by cwon              #+#    #+#             */
+/*   Updated: 2026/03/18 13:21:42 by cwon             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP_
+#define BUREAUCRAT_HPP_
+
+#include <exception>
+#include <string>
+
+class Bureaucrat {
+  public:
+    Bureaucrat(const std::string& name, int grade);
+    Bureaucrat(const Bureaucrat& other);
+    Bureaucrat& operator=(const Bureaucrat& other);
+    ~Bureaucrat();
+
+    const std::string& getName() const;
+    int getGrade() const;
+
+    void incrementGrade();
+    void decrementGrade();
+    
+    class GradeTooHighException : public std::exception {
+      public:
+        const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception {
+      public:
+        const char* what() const throw();
+    };
+  
+  private:
+    const std::string name_;
+    int grade_;
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
+
+#endif // BUREAUCRAT_HPP_
---
+++ module05/ex00/main.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwon <cwon@student.42bangkok.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 13:16:15 by cwon              #+#    #+#             */
+/*   Updated: 2026/03/18 13:19:41 by cwon             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "Bureaucrat.hpp"
+
+int main() {
+  try {
+    Bureaucrat a("Alice", 2);
+    std::cout << a << std::endl;
+
+    a.incrementGrade();
+    std::cout << a << std::endl;
+
+    a.incrementGrade();
+  } catch (std::exception &e) {
+    std::cout << "Exception: " << e.what() << std::endl;
+  }
+
+  std::cout << "------------------" << std::endl;
+
+  try {
+    Bureaucrat b("Bob", 150);
+    std::cout << b << std::endl;
+
+    b.decrementGrade();
+  } catch (std::exception &e) {
+    std::cout << "Exception: " << e.what() << std::endl;
+  }
+
+  std::cout << "------------------" << std::endl;
+
+  try {
+    Bureaucrat c("Charlie", 0);
+  } catch (std::exception &e) {
+    std::cout << "Exception: " << e.what() << std::endl;
+  }
+
+  return 0;
+}
